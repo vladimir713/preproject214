74d67d286ea4552ab1a7e73b19bbfeab9edcdb81
vladimir713 <vladimir713@mail.ru>
2022-09-18 15:37:49 +0300

    Смерть Кащею

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,5 +1,7 @@
 package koschei.config;
 
+import koschei.models.Duck5;
+import koschei.models.Egg6;
 import koschei.models.Island2;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
@@ -10,8 +12,11 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
 
-    @Bean
+    @Bean("island")
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
+
+    @Bean("duck")
+    public static Duck5 getDuck(Egg6 egg) {return new Duck5(egg);}
 }
--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,11 +1,13 @@
 package koschei.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
     @Autowired
+    @Qualifier("duck")
     private Duck5 duck;
     @Override
     public String toString() {
